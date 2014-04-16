e3e375eacdf5e7d01d7b538a2ad37c6da6ef8392
Adam Lindberg <hello@alind.io>
2014-04-16 10:34:45 +0200

    Add git_prompt_behind and git_prompt_exists
    This adds two new theme functions for git:
    * `git_prompt_behind` works in a identical fashion to `git_prompt_ahead`
      and will output a format variable (`ZSH_THEME_GIT_PROMPT_BEHIND`) if
      the branch is behind.
    * `git_prompt_remote` will output one format variable if the branch
      exists on remote (`ZSH_THEME_GIT_PROMPT_REMOTE_EXISTS`), and another
      if the branch is unpublished (`ZSH_THEME_GIT_PROMPT_REMOTE_MISSING`).
    
    The old `git_prompt_ahead` has been changed. Using git log is subject
    to formatting in .gitconfig, which can be overridden and will break
    this function. Relying on rev-list is much more stable.

--- lib/git.zsh
+++ lib/git.zsh
@@ -59,10 +59,34 @@ git_remote_status() {
     fi
 }
 
-# Checks if there are commits ahead from remote
+# Gets the number of commits ahead from remote
+function git_commits_ahead() {
+  if $(echo "$(command git log @{upstream}..HEAD 2> /dev/null)" | grep '^commit' &> /dev/null); then
+    COMMITS=$(command git log @{upstream}..HEAD | grep '^commit' | wc -l | tr -d ' ')
+    echo "$ZSH_THEME_GIT_COMMITS_AHEAD_PREFIX$COMMITS$ZSH_THEME_GIT_COMMITS_AHEAD_SUFFIX"
+  fi
+}
+
+# Outputs if current branch is ahead of remote
 function git_prompt_ahead() {
-  if $(echo "$(command git log origin/$(current_branch)..HEAD 2> /dev/null)" | grep '^commit' &> /dev/null); then
+  if [[ -n "$(command git rev-list origin/$(current_branch)..HEAD 2> /dev/null)" ]]; then
     echo "$ZSH_THEME_GIT_PROMPT_AHEAD"
+  fi
+}
+
+# Outputs if current branch is behind remote
+function git_prompt_behind() {
+  if [[ -n "$(command git rev-list HEAD..origin/$(current_branch) 2> /dev/null)" ]]; then
+    echo "$ZSH_THEME_GIT_PROMPT_BEHIND"
+  fi
+}
+
+# Outputs if current branch exists on remote or not
+function git_prompt_remote() {
+  if [[ -n "$(command git show-ref origin/$(current_branch) 2> /dev/null)" ]]; then
+    echo "$ZSH_THEME_GIT_PROMPT_REMOTE_EXISTS"
+  else
+    echo "$ZSH_THEME_GIT_PROMPT_REMOTE_MISSING"
   fi
 }
 
@@ -150,5 +174,3 @@ function git_compare_version() {
 POST_1_7_2_GIT=$(git_compare_version "1.7.2")
 #clean up the namespace slightly by removing the checker function
 unset -f git_compare_version
-
-
